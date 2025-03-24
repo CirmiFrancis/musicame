26535ff85b4ec8b19c1e965c2eea868afee2d0bd
Francis Cirmi <cirmifrancis@gmail.com>
2025-03-24 14:20:19 -0300

    Actualizar script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,12 +3,3 @@ import { jugar } from "./play.js";
 
 musica();
 jugar();
-
-/*
-    1) Lista de canciones
-    2) Crear otra lista con 8 canciones (eliminar 2 aleatoriamente)
-    3) Elegir 2 canciones aleatoriamente y mostrar
-    4) El usuario elige la canción ganadora (la ganadora pasa a otra lista)
-    5) La cancion ganadora pasa de ronda
-    6) Seleccionar juego
-*/
